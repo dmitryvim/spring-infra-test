0ff648d42a8cc1cf4e47d37631f3a21ed6a98b9a
Dmitry Mikhaylovich <dmitry.mikhailovich@gmail.com>
2021-07-30 16:59:13 +0100

    integration tests added

---
+++ src/intTest/java/com/dmitryvim/resource/HeyResponseTest.java
@@ -0,0 +1,35 @@
+package com.dmitryvim.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class HeyResponseTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void should_use_default_name() {
+        // when
+        var response = restTemplate.getForObject("/hey", HeyResponse.class);
+
+        // then
+        assertThat(response.getName()).isEqualTo("noname");
+    }
+
+    @Test
+    public void should_use_name_from_parametres() {
+        // when
+        var response = restTemplate.getForObject("/hey?name=test", HeyResponse.class);
+
+        // then
+        assertThat(response.getName()).isEqualTo("test");
+    }
+}
