f3e8b84139917978c2225dee295f8eeebab63792
Dmitry Mikhaylovich <dmitry.mikhailovich@gmail.com>
2021-07-30 16:24:34 +0100

    lombok setup

--- src/main/java/com/dmitryvim/resource/HelloWorldResource.java
+++ src/main/java/com/dmitryvim/resource/HelloWorldResource.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class HelloWorldResource {
 
     @GetMapping("/hey")
-    public String hey(@RequestParam(value = "name", required = false) String name) {
-        return name == null ? "Hey noname" : ("Glad to meet you " + name);
+    public HeyResponse hey(@RequestParam(value = "name", required = false) String name) {
+        return new HeyResponse(name == null ? "noname" : name, new Random().nextInt());
     }
 }
---
+++ src/main/java/com/dmitryvim/resource/HeyResponse.java
@@ -0,0 +1,11 @@
+package com.dmitryvim.resource;
+
+import lombok.Value;
+
+@Value
+public class HeyResponse {
+
+    private final String name;
+    private final int count;
+
+}
